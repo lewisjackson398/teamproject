d2141f1cc7f243531cdb4304cacf5e5b2800b5ae
lewisjackson398 <45105851+lewisjackson398@users.noreply.github.com>
2020-04-10 19:32:33 +0100

    Update schedule.js

--- resources/js/schedule.js
+++ resources/js/schedule.js
@@ -1,44 +1,41 @@
-    function myFunction() {
-        var input, filter, table, tr, td, i, txtValue;
-        input = document.getElementById("myInput");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("test");
-        tr = table.getElementsByTagName("tr");
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[0];
-            if (td) {
-                txtValue = td.textContent || td.innerText;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                };
+//search classes found in database
+$('.search-box input[type="text"]').on("keyup input", function () {
+    /* Get input value on change */
+    var inputVal = $(this).val();
+    var resultDropdown = $(this).siblings(".result");
+    if (inputVal.length) {
+        $.get("backend-search.php", {
+            class: inputVal
+        }).done(function (data) {
+            // Display the returned data in browser
+            resultDropdown.html(data);
+        });
+    } else {
+        resultDropdown.empty();
+    }
+});
+// Set search input value on click of result item
+$(document).on("click", ".result p", function () {
+    $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
+    $(this).parent(".result").empty();
+});
+
+//hide elements that dont appear in search
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("test");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
             };
         };
-    }
-
-
-$(document).ready(
-    function () {
-        $('.search-box input[type="text"]').on("keyup input", function () {
-            /* Get input value on change */
-            var inputVal = $(this).val();
-            var resultDropdown = $(this).siblings(".result");
-            if (inputVal.length) {
-                $.get("backend-search.php", {
-                    class: inputVal
-                }).done(function (data) {
-                    // Display the returned data in browser
-                    resultDropdown.html(data);
-                });
-            } else {
-                resultDropdown.empty();
-            }
-        });
-
-        // Set search input value on click of result item
-        $(document).on("click", ".result p", function () {
-            $(this).parents(".search-box").find('input[type="text"]').val($(this).text());
-            $(this).parent(".result").empty();
-        });
-    });
+    };
+}
