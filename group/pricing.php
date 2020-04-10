ef28f372bd0d94fde1690bc8e7dbd70ea6d9de14
Tempusvaito <47416178+Tempusvaito@users.noreply.github.com>
2020-04-10 16:34:46 +0100

    Update pricing.php

--- group/pricing.php
+++ group/pricing.php
@@ -66,17 +66,17 @@ echo makeNav();
          <div class="row">
             <div class="col-md-4 col-sm-4">
                <div class="price-box">
-                  <input type="submit" class="btn btn-primary btn-xl" value="Select">
+                  <input href="../membership/membership.php" type="submit" class="btn btn-primary btn-xl" value="Select">
                </div>
             </div>
             <div class="col-md-4 col-sm-4">
                <div class="price-box">
-                  <input type="submit" class="btn btn-primary btn-xl" value="Select">
+                  <input href="../membership/membership.php" type="submit" class="btn btn-primary btn-xl" value="Select">
                </div>
             </div>
             <div class="col-md-4 col-sm-4">
                <div class="price-box">
-                  <input type="submit" class="btn btn-primary btn-xl" value="Select">
+                  <input href="../membership/membership.php" type="submit" class="btn btn-primary btn-xl" value="Select">
                </div>
             </div>
          </div>
