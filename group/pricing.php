844ae0a8b0ffbccd572b50c9a98fd15e73aa75b6
Tempusvaito <47416178+Tempusvaito@users.noreply.github.com>
2020-04-10 16:39:15 +0100

    Update pricing.php

--- group/pricing.php
+++ group/pricing.php
@@ -66,17 +66,17 @@ echo makeNav();
          <div class="row">
             <div class="col-md-4 col-sm-4">
                <div class="price-box">
-                  <input type="submit" class="btn btn-primary btn-xl" value="Select" onClick="<?php header("Location: ..membership/membership.php"); ?>">
+                  <input type="submit" class="btn btn-primary btn-xl" value="Select" onClick="<?php header("..membership/membership.php"); ?>">
                </div>
             </div>
             <div class="col-md-4 col-sm-4">
                <div class="price-box">
-                  <input type="submit" class="btn btn-primary btn-xl" value="Select" onClick="<?php header("Location: ..membership/membership.php"); ?>">
+                  <input type="submit" class="btn btn-primary btn-xl" value="Select" onClick="<?php header("..membership/membership.php"); ?>">
                </div>
             </div>
             <div class="col-md-4 col-sm-4">
                <div class="price-box">
-                  <input type="submit" class="btn btn-primary btn-xl" value="Select" onClick="<?php header("Location: ..membership/membership.php"); ?>">
+                  <input type="submit" class="btn btn-primary btn-xl" value="Select" onClick="<?php header("..membership/membership.php"); ?>">
                </div>
             </div>
          </div>
