e8593bd14c52004273a1fd4166b3bf7d03f1b813
Tempusvaito <47416178+Tempusvaito@users.noreply.github.com>
2020-04-18 20:15:31 +0100

    Update welcome.php

--- group/welcome.php
+++ group/welcome.php
@@ -3,13 +3,11 @@ session_start();
 include('global/makeHeader.php');
 echo makeHeader();
 
-
 // Check if the user is logged in, if not then redirect him to login page
 if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: login.php");
     exit;
 }
-
 
 ?>
 
