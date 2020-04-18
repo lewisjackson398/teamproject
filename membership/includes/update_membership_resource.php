df2423e52e9bedaa71d6e69b0c64d6d89a779081
Tempusvaito <47416178+Tempusvaito@users.noreply.github.com>
2020-04-18 14:24:47 +0100

    Update update_membership_resource.php

--- membership/includes/update_membership_resource.php
+++ membership/includes/update_membership_resource.php
@@ -2,9 +2,10 @@
 // Include config file
 require_once("config.php");
 
-
+// If the user clicked submit button on the form
 if(isset($_POST["submit"]))
 {
+    // Store the form data in variables
     $age = $_POST['age'];
     $gender = $_POST['gender'];
     $membership_type = $_POST['membership_type'];
@@ -12,51 +13,88 @@ if(isset($_POST["submit"]))
     $price = $_POST['price'];
     $user_id = $_POST['user_id'];
 
+    // Query for updating the member table with the new values 
     $update_member = "UPDATE tblmember 
                         SET age = '$age', gender = '$gender', membership_type = '$membership_type'
                             WHERE user_id = '$user_id'";
 
+    // Run the above query
     if(mysqli_query($link, $update_member))
     {
-        echo "Success! Your membership has been updated"; 
+        updateMembershipTrue();
     }
     else
     {
-        echo "Sorry, something must have gone wrong. Please try again later.";
+        updateMembershipFalse();
     }
 
+    // Query to get member details from the memebr table where the user_id is equal to the session user_id
     $get_member_details = "SELECT member_id, end_of_membership_date FROM tblmember WHERE user_id = '$user_id'";
 
+    // Store the result of the query above in a variable
     $member_result = mysqli_query($link, $get_member_details);
 
+    // Store the result above as an array
     $member_rows = mysqli_fetch_array($member_result, MYSQLI_NUM);
 
+    // Store the first object in the array, which is the member_id, as a variable
     $member_id = $member_rows[0];
 
+    // Store the second object in the array, which is the end of membership date, as a variable
     $membership_end = $member_rows[1];
 
+    // Query for getting the payment time so the new payment can be made at the same time at the end of the membership month
     $get_payment_details = "SELECT payment_time FROM tblpayment WHERE member_id = '$member_id'";
 
+    // Store the result of the above query as a variable
     $payment_result = mysqli_query($link, $get_payment_details);
 
+    // Store the result above as an array
     $payment_rows = mysqli_fetch_array($payment_result, MYSQLI_NUM);
 
+    // Store the first object in the array, which is the payment time, as a variable
     $payment_time = $payment_rows[0];
 
+    // Query for inserting the new payment details and date of payment in the payment table
     $new_payment = "INSERT INTO tblpayment (member_id, type, amount, payment_time, payment_date)
                         VALUES ('$member_id', '$payment_type', '$price', '$payment_time', '$membership_end')";
 
+    // Run the above query
     if(mysqli_query($link, $new_payment))
     {
-        echo "Success! Your new payment details have been updated and you will be billed at the end of your current membership, for the package you selected.";
+        updatePaymentTrue();
     }
     else
     {
-        echo "Something was wrong with your payment. Please try again later." . mysqli_error($link);
+        updatePaymentFalse();
     }
 
+    // Close the database connection
     mysqli_close($link);
 
-    header("location: ../group/login.php");
+    // Redirect to the login page, if the user is still logged in, then they will be redirected to the welcome page
+    header("location: ../teamproject/group/login.php");
 }
 ?>
+
+<script>
+    function updateMembershipTrue()
+    {
+        alert("Your membership has been updated");
+    }
+
+    function updateMembershipFalse()
+    {
+        alert("Something went wrong with updating your membership, please try again later");
+    }
+
+    function updatePaymentTrue()
+    {
+        alert("Your new payment details have been added, you will be billed at the end of your current membership package");
+    }
+
+    function updatePaymentFalse()
+    {
+        alert("Something went wrong with your payment, please try again later");
+    }
+</script>
