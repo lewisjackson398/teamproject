7f6e0c32a004d9b76ddb5b8a493cd454f6463c70
olih123 <47420633+olih123@users.noreply.github.com>
2020-04-29 18:04:06 +0100

    Update forum.php

--- forum/forum.php
+++ forum/forum.php
@@ -22,113 +22,22 @@ echo makeHeader();
     <br><br>
     <section class="forum_cont">
         <div class="container">
-            
+            <form action="register.php" method ="POST"> 
+                Username: <input type="text" name="username">
+                <br />Password: <input type ="Password" name="username"> 
+                <br />Confirm password: <input type="password" name="username"> 
+                <br />Email: <input type="text" name="username">
+                <br /><input type="submit" name="submit" value="Register"> or <a href="login.php">Login</a>
+            </form>
         
-
 
         </div>
     </section>    
 
-                    <div id="category_1"class="row forumModule forumMargin">
-                        <a href="#" onlick="showDiscussions(1); ">
-                            <div class = "col-lg-12 columnPad">
-                                <div class ="pullLeft" >
-                                    <h2>Category Name</h2> 
-                                </div>
-                                    <div class="pullRight"> 
-                                        <a id="startDiscussionButton_1" class="button" href="#" onclick="startDiscussion(1);"> Start Discussion </a>
-                                    </div>
-                                </div> 
-                            </div>     
-                        </a>
-                    </div>
 
-                    <div id="cat_discussions_1" class="row forumPad" style="display:none;">
-                        <div class="col-lg-12">
-                            <form>
-                                <div class="form-group">
-                                    Title
-                                    <input type="text" name="newDiscussionTitle" class ="form-control" size="40"></input>
-                                    <textarea class="newDiscussionText" name="newDiscussionText" rows="10"></textarea>
-                                </div>
-                            </form>
-                    </div>    
-
-                        
-                    <div id="cat_discussions_1" class="row forumPad" style="display:none;">
-                    <div class="col-lg-12">
-                        <table class ="forumTable">
-                            <thead>
-                                <tr>
-                                    <tr>
-                                        Discussion 
-                                    </tr>
-                                    <tr>
-                                        Author -- poster 
-                                    </tr>
-                                    <tr>
-                                        Replies -- Amount
-                                    </tr>
-                                </tr>
-                            </thead>
-                            <td>
-                                <a href="a"> Discussion name here </a>
-                            </td>
-                            <td>
-                                <a href="a"> Author Name </a>
-                            </td>
-                            <td>
-                                25
-                            </td>
-                        </table>
-                        </div>
-                    </div>
-                        <div class="row">
-                            <div class ="col-lg 12">
-                                <div class="forumPad pullRight" style="padding-right:15px;">
-                                    <a class="label label-primary" href="a">View More</a>
-                                </div>
-                            </div> 
-                        </div>   
-                    </div>    
-
-
-
-                
-
-
-
-
-
-
-                </body> 
-
-                <script>
-                    function showDiscussions(cat_id){
-                    var html_id = "#cat_discussions_" + cat_id; 
-                    if($(html_id).css("display", "block") == 'block'){
-                        $(html_id).css("display", "none");
-                    }else{
-                        $(html_id).css("display", "block");
-                    }
-                }
-
-                function startDiscussion(cat_id){
-                    var html_id = "#cat_new_discussion_1" + cat_id;
-                    if($(html_id).css("display", "block") == 'block'){
-                        $(html_id).css("display", "none");
-                    }else{
-                        $(html_id).css("display", "block");
-                    }
-                }
-
-                </script>
-
-                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" type="text/javascript"></script>
-                <script src="https://code.jquery.com/jquery-3.5.0.min.js"  integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
-                <script src="themes/default/js/forum_home.js" type="text/javascript"></script>
 
                     <?php
                     include('../group/global/makeFooter.php');
                     echo makeFooter();
                     ?>
+ 
