b622c266de53aa96302e6db2cdb841957ac8bea0
Tempusvaito <47416178+Tempusvaito@users.noreply.github.com>
2020-04-30 17:43:49 +0100

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -2,8 +2,8 @@
 <?php
 // Include config file
 require_once "../server/config/config.php";
-$class_id=$_REQUEST['class_id'];
-$query = "DELETE FROM tblclasses WHERE class_id=$class_id"; 
-$result = mysqli_query($con,$query) or die ( mysqli_error());
+$class_id = $_GET['class_id'];
+$query = "DELETE FROM tblclasses WHERE class_id = '$class_id'"; 
+$result = mysqli_query($link, $query) or die ( mysqli_error());
 header("Location: view.php"); 
 ?>
