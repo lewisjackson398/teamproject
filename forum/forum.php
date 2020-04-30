01e9b4db0d838a67c3c13cae0b257918417591a2
olih123 <47420633+olih123@users.noreply.github.com>
2020-04-30 12:59:49 +0100

    Update forum.php

--- forum/forum.php
+++ forum/forum.php
@@ -1,15 +1,13 @@
 <?php
 session_start();
 
-//require_once('includes/membership_resource.php');
-
-// Check if the user is logged in, if not then redirect him to login page
-//if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) 
-//{
-//    echo "It looks like you're not logged in, please login.";
-//    header("location: ../group/login.php");
-//    exit;
-//}
+Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) 
+{
+    echo "It looks like you're not logged in, please login.";
+    header("location: ../group/login.php");
+    exit;
+}
 
 include('../group/global/makeHeader.php');
 echo makeHeader();
