fe9f4d1fe9d477c913e3b203021e5824c2e6f891
ArlanaKeen97 <47416180+ArlanaKeen97@users.noreply.github.com>
2020-04-30 13:20:38 +0100

    Update delete.php

--- admin/delete.php
+++ admin/delete.php
@@ -1,8 +1,9 @@
 
 <?php
-require('db.php');
-$id=$_REQUEST['id'];
-$query = "DELETE FROM new_record WHERE id=$id"; 
+// Include config file
+require_once "../server/config/config.php";
+$class_id=$_REQUEST['class_id'];
+$query = "DELETE FROM tblclasses WHERE class_id=$class_id"; 
 $result = mysqli_query($con,$query) or die ( mysqli_error());
 header("Location: view.php"); 
 ?>
