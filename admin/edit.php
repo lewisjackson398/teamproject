22136a114bbd7cc7dbf104ae95379d0914462f41
ArlanaKeen97 <47416180+ArlanaKeen97@users.noreply.github.com>
2020-04-30 13:20:43 +0100

    Update edit.php

--- admin/edit.php
+++ admin/edit.php
@@ -32,11 +32,11 @@ if(isset($_POST['new']) && $_POST['new']==1)
     $finish = $_REQUEST['finish'];
     $instructor_name = $_REQUEST['instructor_name'];
 $update="update tblclasses class_id='".$class_id."',
-name='".$name."', age='".$age.",',
-submittedby='".$submittedby."' where id='".$id."'";
+user_id='".$user_id."', date='".$date."',
+class='".$class."',start='".$start."',finish='".$finish."', instructor_name'".$instructor_name."' where class_id='".$class_id."'";
 mysqli_query($con, $update) or die(mysqli_error());
 $status = "Record Updated Successfully. </br></br>
-<a href='view.php'>View Updated Record</a>";
+<a href='view.php'>View Updated class</a>";
 echo '<p style="color:#FF0000;">'.$status.'</p>';
 }else {
 ?>
@@ -44,10 +44,21 @@ echo '<p style="color:#FF0000;">'.$status.'</p>';
 <form name="form" method="post" action=""> 
 <input type="hidden" name="new" value="1" />
 <input name="id" type="hidden" value="<?php echo $row['id'];?>" />
-<p><input type="text" name="name" placeholder="Enter Name" 
-required value="<?php echo $row['name'];?>" /></p>
-<p><input type="text" name="age" placeholder="Enter Age" 
-required value="<?php echo $row['age'];?>" /></p>
+<p><input type="text" name="class_id" placeholder="Enter class_id" 
+required value="<?php echo $row['class_id'];?>" /></p>
+<p><input type="text" name="user_id" placeholder="Enter user_id" 
+required value="<?php echo $row['user_id'];?>" /></p>
+<p><input type="text" name="date" placeholder="Enter date" 
+required value="<?php echo $row['date'];?>" /></p>
+<p><input type="text" name="class" placeholder="Enter class" 
+required value="<?php echo $row['class'];?>" /></p>
+<p><input type="text" name="start" placeholder="Enter start time" 
+required value="<?php echo $row['start'];?>" /></p>
+<p><input type="text" name="finish" placeholder="Enter finish time" 
+required value="<?php echo $row['finish'];?>" /></p>
+<p><input type="text" name="instructor_name" placeholder="Enter instructor name" 
+required value="<?php echo $row['instructor_name'];?>" /></p>
+
 <p><input name="submit" type="submit" value="Update" /></p>
 </form>
 <?php } ?>
