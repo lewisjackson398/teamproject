5607804ebcbf2515437466fd372ed3b8713d5e25
olih123 <47420633+olih123@users.noreply.github.com>
2020-04-30 16:52:31 +0100

    Update forum.php

--- forum/forum.php
+++ forum/forum.php
@@ -24,16 +24,73 @@ echo makeHeader();
             <h2>Metro Gym Forum</h2>
             <p>Want to add to the discussion?</p>
                 <!-- <input onclick="()" type = "button" class="btn btn-success" value="create post" name="create post"> --- Might use might not--> 
-
+        </div>
+    </section>
+                    <!-- button for post trigger --> 
                     <button class="btn btn-success" data-toggle="modal" data-target="#myModalNorm">
-                        Create post
+                        Create post 
                     </button>
 
+                    <!-- Code for post trigger --> 
+
+                    <div class="modal fade" id="myModalNorm" tabindex="-1" role="dialog" 
+                        aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <!-- Modal Header -->
+                                <div class="modal-header">
+                                    <button type="button" class="close" 
+                                    data-dismiss="modal">
+                                        <span aria-hidden="true">&times;</span>
+                                        <span class="sr-only">Close</span>
+                                    </button>
+                                    <h4 class="modal-title" id="myModalLabel">
+                                        Modal title
+                                    </h4>
+                                </div>
+                                
+                                <!-- Modal Body -->
+                                <div class="modal-body">
+                                    
+                                    <form role="form">
+                                    <div class="form-group">
+                                        <label for="exampleInputEmail1">Email address</label>
+                                        <input type="email" class="form-control"
+                                        id="exampleInputEmail1" placeholder="Enter email"/>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="exampleInputPassword1">Password</label>
+                                        <input type="password" class="form-control"
+                                            id="exampleInputPassword1" placeholder="Password"/>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox"/> Check me out
+                                        </label>
+                                    </div>
+                                    <button type="submit" class="btn btn-default">Submit</button>
+                                    </form>
+                                    
+                                    
+                                </div>
+                                
+                                <!-- Modal Footer -->
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default"
+                                            data-dismiss="modal">
+                                                Close
+                                    </button>
+                                    <button type="button" class="btn btn-primary">
+                                        Save changes
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
                 
            
-        </div>
-    </section>
+      
    
 </body>    
 <html>
