a5cfc09211168d0080605615270c938ca2f6e8aa
ArlanaKeen97 <47416180+ArlanaKeen97@users.noreply.github.com>
2020-04-30 13:06:19 +0100

    Create Classes.php

---
+++ admin/Classes.php
@@ -0,0 +1,22 @@
+<?php
+// Include config file
+require_once "../server/config/config.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Classes</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>Welcome to the classes dashboard.</p>
+<p><a href="admin.php">Home</a><p>
+<p><a href="insert.php">Insert New Record</a></p>
+<p><a href="view.php">View Records</a><p>
+<p><a href="adminLogout.php">Logout</a></p>
+</div>
+</body>
+</html>
+    
