267f27c579eeab08fc6f50f04a265abe1b0c7705
olih123 <47420633+olih123@users.noreply.github.com>
2020-05-01 21:24:46 +0100

    awfw

---
+++ forum/create_topic_parse.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) 
+{
+    echo "It looks like you're not logged in, please login.";
+    header("location: ../group/login.php");
+    exit;
+}
+
+//Initialize the session
+include('../group/global/makeHeader.php');
+echo makeHeader();
+
+if (isset($_POST['topic_submit'])) {
+    if (($_POST['topic_title'] == "") && ($_POST['topic_content'] == "")){
+        echo "You must fill in both fields to post a topic";
+        exit();
+    } else{
+        include_once("../forum/includes/config.php");
+        $cid = $_POST['cid'];
+        $title = $_POST['topic_title'];
+        $content = $_POST['topic_content'];
+        $creator = $_SESSION['loggedin'];
+        $sql = "INSERT INTO tbltopics (category_id, topic_title, topic_creator, topic_date, topic_reply_date) 
+        VALUES ('".$cid."', '".$title."', '".$creator."', now(), now())";
+        $result = mysqli_query($link, $sql) or die (mysqli_error($link));
+        $new_topic_id = mysqli_insert_id(connection);
+        $sql2 = "INSERT INTO posts (category_id, topic_id, post_creator, post_content, post_date) 
+        VALUES ('".$cid."', '".$new_topic_id."', '".$creator."', '".$content."', now(), now())"; 
+        $result2 = mysqli_query($link, $sql2) or die(mysqli_error($link));
+        $sql3 = "UPDATE categories SET last_post_date=now(), last_user_posted='".$creator."' WHERE id='".$cid."' LIMIT 1"; 
+        $result3 = mysqli_query($link, $sql2) or die(mysqli_error($link));
+        if (($result1) && ($result2) && ($result3)) {
+            header("Location: view_topic.php?cid=".$cid."$tid=".$new_topic_id);             
+        } else {
+            echo "A problem occured when creating your topic. Please try again"; 
+        }
+    }
+}
+
+
+?>
--- forum/forum.php
+++ forum/forum.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 
-
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) 
 {
@@ -13,15 +12,16 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
 //Initialize the session
 include('../group/global/makeHeader.php');
 echo makeHeader();
-
-// this was under body but at the top - might require putting back there.
-//<?php include('../group/global/makeNav.php');
-//echo makeNav();
 ?>
 
-
+<head>
+    <title>Forum Index </title>
+</head>
 
 <body id="page-top" class="page forum">
+<?php include('../group/global/makeNav.php');
+    echo makeNav();
+?>
   
 <section class="forum">
     <div class="text-center">
@@ -35,31 +35,31 @@ echo makeHeader();
                 <br>
             </div>
         </div>
-        <div id="content">
-        <?php
-        include('../server/config/config.php');
-        $sql = "SELECT * FROM tblcategories ORDER BY category_title ASC";
-        $result = mysqli_query($link, $sql) or die (mysqli_error(connection));
-        $categories =""; 
-        if (mysqli_num_rows($result) > 0){
-            while ($row = mysqli_fetch_assoc($result)){
-                $id = $row['id']; 
-                $title = $row['category_title'];
-                $description = $row['category_description']; 
-                $categories .="<a href='#' class = 'cat_links'>.$title. - <font size=' -1'></font>".$description."</a>";
-            }
-            echo $categories;
-        } else {
-                echo "<p> Currently there are no categories are available. </p>";
-            }
-        ?> 
-
+        <div class="row">
+            <div id="content">
+                <div style = "text-align: center;">
+                    <?php
+                    include('../server/config/config.php');
+                    $sql = "SELECT * FROM tblcategories ORDER BY category_title ASC";
+                    $result = mysqli_query($link, $sql) or die (mysqli_error(connection));
+                    $categories =""; 
+                    if (mysqli_num_rows($result) > 0){
+                        while ($row = mysqli_fetch_assoc($result)){
+                            $id = $row['id']; 
+                            $title = $row['category_title'];
+                            $description = $row['category_description']; 
+                            $categories .="<a href='view_category.php?cid=".$id."' class = 'cat_links'>.$title. - <font size=' -1'></font>".$description."</a>";
+                        }
+                        echo $categories;
+                    } else {
+                            echo "<p> Currently there are no categories are available. </p>";
+                        }
+                    ?>
+                </div>     
+            </div>  
+        </div>
     </div>       
 </section>    
-
-
-
-
 
     
 <?php
