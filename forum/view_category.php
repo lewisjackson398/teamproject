dd730846a3dcff9f3e89637157039870cafb2a94
olih123 <47420633+olih123@users.noreply.github.com>
2020-05-02 16:52:34 +0100

    wfaw

--- forum/view_category.php
+++ forum/view_category.php
@@ -40,6 +40,7 @@ echo makeHeader();
                     <?php
                         include_once("../forum/includes/config.php");
                         $cid = $_GET['cid'];
+                        $topics ="";
 
                         if (isset($_SESSION['loggedin'])){
                             $logged = " | <a href='create_topic.php?cid=".$cid."'>Click Here To Create A Topic</a>";
@@ -51,31 +52,29 @@ echo makeHeader();
                         if (mysqli_num_rows($result) == 1){
                             $sql2 = "SELECT * FROM tbltopics WHERE category_id='".$cid."' ORDER BY topic_reply_date DESC";
                             $result2 = mysqli_query($link, $sql2) or die (mysqli_error($link)); 
-
-                                                               // $topics = "";// -------------- Might be bad later on 
-
+                           
                             if (mysqli_num_rows($result2) > 0){
-                                $tbltopics .= "<table width = 100%' style='border-collapse: collapse;'>";
-                                $tbltopics .= "<tr><td colspan ='3'><a href='forum.php'>Return to Forum Index Page</a>".$logged."<hr /></td></tr>";
-                                $tbltopics .= "<tr style='background-color: #dddddd;'><td>Topic Title</td><td width='65' align='center'>Replies</td>
+                                $topics .= "<table width = 100%' style='border-collapse: collapse;'>";
+                                $topics .= "<tr><td colspan ='3'><a href='forum.php'>Return to Forum Index Page</a>".$logged."<hr /></td></tr>";
+                                $topics .= "<tr style='background-color: #dddddd;'><td>Topic Title</td><td width='65' align='center'>Replies</td>
                                 <td width='65' align='center'>Views</td><tr>";
-                                $tbltopics .= "<tr><td colspan='3'><hr /></td><tr>";
+                                $topics .= "<tr><td colspan='3'><hr /></td><tr>";
                                 while ($row = mysqli_fetch_assoc($result2)) {
                                     $tid = $row['id'];
                                     $title = $row['topic_title'];
                                     $views = $row['topic_views'];
                                     $date = $row['topic_date'];
                                     $creator = $row['topic_creator'];
-                                    $tbltopics .= "<tr><td><a href='view_topic.php?cid=".$cid."&tid=".$tid."'>".$title."</a><br /><span class='post_info'>Posted
+                                    $topics .= "<tr><td><a href='view_topic.php?cid=".$cid."&tid=".$tid."'>".$title."</a><br /><span class='post_info'>Posted
                                     by: ".$creator." on ".$date."</span></td><td align='center'>0</td><td align='center'>".$views."</td></tr>";
-                                    $tbltopics .= "<tr><td colspan='3'><hr /></td><tr>";
+                                    $topics .= "<tr><td colspan='3'><hr /></td><tr>";
                                 }
-                                $tbltopics .= "</table>";
-                                echo $tbltopics;
-                            } else {
-                                echo "<a href='forum.php'>Return To Forum Index Page</a><hr />";
-                                echo "<p>No Topics exist in this category.".$logged."</p>";
-                            }
+                                $topics .= "</table>";
+                                echo $topics;
+                                } else {
+                                    echo "<a href='forum.php'>Return To Forum Index Page</a><hr />";
+                                    echo "<p>No Topics exist in this category.".$logged."</p>";
+                                }
 
                             } else {
                                 echo "<a href='forum.php'>Return To Forum Index Page</a><hr />";
--- forum/view_topic.php
+++ forum/view_topic.php
@@ -47,13 +47,14 @@ echo makeHeader();
                         if(mysqli_num_rows($result) == 1) {
                             echo "<table width='100%'>";
                             if ($_SESSION['loggedin']) { echo "<tr><td colspan ='2'><input type='submit' value='Add Reply' onClick=\"window.location =
-                                post_reply.php?cid=".$cid."&tid=".$tid."\" /><hr />"; } else {echo "<tr><td colspan='2'><p>Please log in to add reply</p><hr /></td></tr>";}
+                                'post_reply.php?cid=".$cid."&tid=".$tid."'\" /><hr />"; } else {echo "<tr><td colspan='2'><p>Please log in to add reply</p><hr /></td></tr>";}
                                 while ($row = mysqli_fetch_assoc($result)){
                                     $sql2 = "SELECT * FROM posts WHERE category_id='".$cid."' AND topic_id='".$tid."'";
                                     $result2 = mysqli_query($link, $sql2) or die (mysqli_error($link));
                                     while ($row2 = mysqli_fetch_assoc($result2)){
-                                        echo "<tr><td valign='top' style='border: 1px solid #000000;'><div style='min-height: 125px;'>".$row['topic_title']."<br /> align='center' 
-                                        style='border: 1px solid #000000;'>User Info Here</td></tr><tr><td colspan='2'><hr /></td></tr>";
+                                        echo "<tr><td valign='top' style='border: 1px solid #000000;'><div style='min-height: 125px;'>".$row['topic_title']."<br /> 
+                                        by ".$row2['post_creator']." - ".$row2['post_date']."<hr />".$row2['post_content']."</div></td><td width='200' valign ='top'
+                                        align='center' style='border: 1px solid #000000;'>User Info Here</td></tr><tr><td colspan='2'><hr /></td></tr>";
                                     }
                                 }
                         } else {
