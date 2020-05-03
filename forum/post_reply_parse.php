fdcebf37d07517098be0f07662c070176e808d55
olih123 <47420633+olih123@users.noreply.github.com>
2020-05-03 15:39:06 +0100

    Update post_reply_parse.php

--- forum/post_reply_parse.php
+++ forum/post_reply_parse.php
@@ -6,10 +6,15 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
 {
     if (isset($_POST['reply_submit'])){
         include_once("config.php"); 
+         
         $creator = $_SESSION['loggedin'];
+        echo "$creator";
         $cid = $_POST['cid'];
+        echo "$cid";
         $tid = $_POST['tid'];
+        echo = $_tid;
         $reply_content = $_POST['reply_content'];
+        echo = $reply_content; 
         $sql = "INSERT INTO posts (category_id, topic_id, post_creator, post_content, post_date)
         VALUES ('".$cid."', '".$tid."', '".$creator."', '".$reply_content."', now())";
         $result = mysqli_query($link, $sql) or die (mysql_error($link));
@@ -20,25 +25,25 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
 
         //EMAIL SENDING
 
-        if (($result) && ($result2) && ($result3)) {
-            echo "<p>Your reply has been succesfully posted. <a href='view_topic.php?cid=".$cid."&tid=".$tid."'>Click here to return to topic home.</a></p>";   
-        } else {
-            echo "<p>There was a problem with posting your reply, try again.</p>";            
-        }
+            if (($result) && ($result2) && ($result3)) {
+                echo "<p>Your reply has been succesfully posted. <a href='view_topic.php?cid=".$cid."&tid=".$tid."'>Click here to return to topic home.</a></p>";   
+            } else {
+                echo "<p>There was a problem with posting your reply, try again.</p>";            
+            }
 
-        echo "It looks like you're not logged in, please login.";
-        header("location: ../group/login.php");
-        exit;
-        }
+            echo "It looks like you're not logged in, please login.";
+            header("location: ../group/login.php");
+            exit;
+            }
 
         //Initialize the session
         include('../group/global/makeHeader.php');
         echo makeHeader();
-     
-        } else {
-
-            exit();
- 
+} 
+else {
+            
+    exit();
 }
+echo "Test break"; 
 
 ?> 
