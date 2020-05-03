3be8fed7780b3bc8ab9e556d845c514178a38ff8
ArlanaKeen97 <47416180+ArlanaKeen97@users.noreply.github.com>
2020-05-03 10:45:35 +0100

    Create equipment.php

---
+++ admin/equipment.php
@@ -0,0 +1,22 @@
+<?php
+// Include config file
+require_once "../server/config/config.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Equipment</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>Welcome to the equipment dashboard.</p>
+<p><a href="classes.php">Back to classes</a><p>
+<p><a href="viewEquipment.php">View equipment booked</a></p>
+<p><a href="insertEquipment.php">Book Equipment</a><p>
+<p><a href="adminLogout.php">Logout</a></p>
+</div>
+</body>
+</html>
+    
