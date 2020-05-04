58cc7c30252cb3852eff8fcf3f1b50d20a7e24c7
olih123 <47420633+olih123@users.noreply.github.com>
2020-05-04 12:50:21 +0100

    Some changes

--- forum/view_topic.php
+++ forum/view_topic.php
@@ -52,8 +52,7 @@ echo makeHeader();
                                     $result2 = mysqli_query($link, $sql2) or die (mysqli_error($link));
                                     while ($row2 = mysqli_fetch_assoc($result2)) {
                                         echo "<tr><td valign='top' style='border: 1px solid #000000;'><div style='min-height: 125px;'>".$row['topic_title']."<br />
-                                        by ".$row2['post_creator']." - ".$row2['post_date']."<hr />".$row2['post_content']."</div></td><td width='200' valign='top'
-                                        align='center' style='border: 1px solid #000000;'>User Infor Here</td></tr><tr><td colspan='2'<hr /></td></tr>";
+                                        by ".$row2['post_creator']." - ".$row2['post_date']."<hr />".$row2['post_content']."</div></td>";
                                     }
                                      
                                     $old_views = $row['topic_views'];
